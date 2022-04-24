0775e1dec416afe6b0381b1c179c111fb91228ce
jkurdziel <jakub.kurdziel@talkie.ai>
2022-04-24 23:41:00 +0200

    add prettier and eslint

--- src/controller/profile/profile-handler.ts
+++ src/controller/profile/profile-handler.ts
@@ -1,37 +1,47 @@
-import {Request, Response, Router} from "express";
-import {Profile} from "../../utils/profile";
+import { Request, Response, Router } from 'express';
+import { Profile } from '../../utils/profile';
 
-
-const router = Router({mergeParams: true});
+const router = Router({ mergeParams: true });
 
 router.get('/', async (req: Request, res: Response) => {
+  const token = req.query['verify_token'];
+  const mode = req.query['mode'];
 
-    const token = req.query["verify_token"];
-    const mode = req.query["mode"];
+  if (mode && token) {
+    if (token === process.env.verifyToken) {
+      const messengerBotProfile = new Profile(
+        process.env.appUrl,
+        process.env.shopUrl,
+      );
 
-    if (mode && token) {
-        if (token === process.env.verifyToken) {
-            const messengerBotProfile = new Profile(process.env.appUrl, process.env.shopUrl);
+      try {
+        await messengerBotProfile.init();
 
-            try {
-                await messengerBotProfile.init()
+        res.status(200).json({
+          success: true,
+          message: 'Successfully verified',
+        });
 
-                res.status(200).json({
-                    "success": true,
-                    "message": "Successfully verified"
-                });
-
-                return
-            } catch (err) {
-                console.log(err.message);
-                return res.status(500).json({message: err.message});
-            }
-        } else {
-            return res.sendStatus(401);
-        }
+        return;
+      } catch (err) {
+        console.log(err.message);
+        return res.status(500).json({ message: err.message });
+      }
     } else {
-        return res.sendStatus(400);
+      return res.sendStatus(401);
     }
+  } else {
+    return res.sendStatus(400);
+  }
 });
 
 export default router;
+
+
+
+
+
+
+
+
+
